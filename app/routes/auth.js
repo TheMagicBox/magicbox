5214a2ddae421b7a742f31e43dd70e41787cc8d7
Omar Aflak <aflakomar@gmail.com>
2021-02-07 22:06:31 +0100

    signup available before registration only

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const controller = require('../controllers/auth')
+const { magicBoxNotRegistered } = require('../middlewares/magicbox')
 const router = express.Router()
 
-router.post('/signup', controller.signUp)
+router.post('/signup', [magicBoxNotRegistered], controller.signUp)
 router.post('/signin', controller.signIn)
 
 module.exports = router
