a1de14e94f37ea629a92f013129b00f85aa6eea1
Omar Aflak <aflakomar@gmail.com>
2021-02-07 21:58:55 +0100

    update doc

--- app/controllers/doc.js
+++ app/controllers/doc.js
@@ -8,7 +8,8 @@ const doc = (() => {
             ['POST', '/api/auth/signup', 'username: String, password: String', 'None', 'Create an account.'],
             ['POST', '/api/auth/signin', 'username: String, password: String', 'None', 'Sign in to an account.'],
             ['GET', '/api/magicbox', '', 'User', 'Get MagicBox details.'],
-            ['POST', '/api/magicbox/register', 'name: String', 'Admin', 'Register MagicBox for a public url.']
+            ['POST', '/api/magicbox', 'name: String, url: String', 'User', 'Add a MagicBox contact.']
+            ['POST', '/api/magicbox/register', 'name: String', 'Admin', 'Register MagicBox for a public url.'],
         ]
     )
     return table.toString()
