1ae6a796797305b4fa8c1342904f4eb27f03a784
Omar Aflak <aflakomar@gmail.com>
2021-02-07 21:12:36 +0100

    cleaned

--- app/io/server/index.js
+++ app/io/server/index.js
@@ -3,8 +3,6 @@ const { verifyToken } = require('../middlewares/auth')
 module.exports = io => {
     io.use(verifyToken)
     io.on('connection', socket => {
-        socket.on('message', message => {
-            console.log(message)
-        })
+        // socket.on(...)
     })
 }
