fc80ace11e25c5729f909a02cbb62b60a9919d4a
Omar Aflak <aflakomar@gmail.com>
2021-02-09 19:13:16 +0100

    update doc

--- app/controllers/doc.js
+++ app/controllers/doc.js
@@ -8,7 +8,7 @@ const doc = (() => {
             ['POST', '/api/auth/signup', 'username: String, password: String', 'Admin', 'Create an account.'],
             ['POST', '/api/auth/signin', 'username: String, password: String', 'None', 'Sign in to an account.'],
             ['GET', '/api/users', '', 'User', 'Get User details.'],
-            ['GET', '/api/users/:username', '', 'None', 'Get User id by name.'],
+            ['GET', '/api/users/:username', '', 'None', 'Get User information by account name.'],
             ['GET', '/api/magicbox', '', 'User', 'Get MagicBox details.'],
             ['GET', '/api/magicbox/contacts', '', 'User', 'Get MagicBox contacts.'],
             ['POST', '/api/magicbox', 'name: String, url: String, account: String', 'User', 'Add a MagicBox contact.'],
@@ -16,7 +16,7 @@ const doc = (() => {
             ['GET', '/api/folders', '', 'User', 'Get folders list.'],
             ['GET', '/api/folders/:folderId', '', 'User', 'Get folder content.'],
             ['POST', '/api/folders', 'name: String', 'User', 'Create a new folder.'],
-            ['POST', '/api/folders/share', 'name: String, origin: {url: String, account: String, folderId: String}, signature: String', 'None', 'Request the creation of a folder for sharing.'],
+            ['POST', '/api/folders/share', 'name: String, origin: {url: String, account: String, folderId: String}, signature: String', 'None', 'Request the creation of a shared folder.'],
             ['PUT', '/api/folders/share/:folderId', 'magicboxIds: [String]', 'User', 'Share a folder.']
         ]
     )
